76f958eb8d43a5808d03fc15df70e3199846c2d7
Alfonso Ricciardelli <alfonso.ricciardelli@icloud.com>
2023-11-13 07:56:43 -0500

    Latest

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{ FontSize: 80 }}>Hello welcome to Big Lemon App</Text>
       <StatusBar style="auto" />
     </View>
   );
